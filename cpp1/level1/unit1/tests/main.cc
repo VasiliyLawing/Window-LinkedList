da977006917c6a6ace476927db7ed9e28d5d5c41
dek <d.kischukov@gmail.com>
2021-11-19 10:57:46 -0500

    .

---
+++ cpp1/level1/unit1/tests/main.cc
@@ -0,0 +1,4 @@
+#define CATCH_CONFIG_MAIN
+#include <ext-libs/catch2/catch.hpp>
+
+// Generates main function for all tests in this directory
---
+++ cpp1/level1/unit1/tests/t1.cc
@@ -0,0 +1,11 @@
+#include <sstream>
+#include <ext-libs/catch2/catch.hpp>
+
+
+
+TEST_CASE("Code tests") {
+
+  SECTION("test1") {
+    REQUIRE(2+2==4);
+  }
+}
