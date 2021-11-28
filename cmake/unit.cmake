95cbcd2dfd84c94ba34b63ccfcc6d5dda2ef7410
dek <d.kischukov@gmail.com>
2021-11-28 09:51:18 -0500

    unit.cmake

---
+++ cmake/unit.cmake
@@ -0,0 +1,17 @@
+file( GLOB EXAMPLE_SOURCES RELATIVE ${unitDir}/examples examples/*.cc )
+foreach( afile ${EXAMPLE_SOURCES} )
+    string( REPLACE ".cc" "" binaryName ${afile} )
+    add_executable( ${artifactPrefix}_${binaryName} examples/${afile} )
+endforeach( afile ${EXAMPLE_SOURCES} )
+
+
+file(GLOB SRC examples/*cc tasks/*.cc tasks/*.hh)
+add_library(${artifactPrefix} ${SRC} ${EXT_LIBS_HEADERS})
+
+
+if(unitNoTests EQUAL 1)
+add_executable(${artifactPrefix}_tests testsMain.cc)
+target_link_libraries(${artifactPrefix}_tests PUBLIC ${artifactPrefix})
+
+catch_discover_tests(${artifactPrefix}_tests)
+endif()
