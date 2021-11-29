d36bd03218ce6359e4fc3d44629d3672f092eec9
dek <d.kischukov@gmail.com>
2021-11-29 16:16:58 -0500

    .

--- cmake/unit.cmake
+++ cmake/unit.cmake
@@ -9,6 +9,16 @@ file(GLOB SRC examples/*cc tasks/*.cc tasks/*.hh)
 add_library(${artifactPrefix} ${SRC} ${EXT_LIBS_HEADERS})
 
 
+if(DEFINED ANYSOLO_BINARY_TASKS)
+    file( GLOB TASKS_SOURCES RELATIVE ${unitDir}/tasks tasks/*.cc )
+
+    foreach( afile ${TASKS_SOURCES} )
+        string( REPLACE ".cc" "" binaryName ${afile} )
+        add_executable( ${artifactPrefix}_${binaryName} tasks/${afile} )
+    endforeach( afile ${TASKS_SOURCES} )
+endif()
+
+
 if(NOT DEFINED unitNoTests)
     add_executable(${artifactPrefix}_tests testsMain.cc)
     target_link_libraries(${artifactPrefix}_tests PUBLIC ${artifactPrefix})
--- utils/tests/test1.cc
+++ utils/tests/test1.cc
@@ -1,9 +1,30 @@
 #include <sstream>
 #include <ext-libs/catch.hpp>
+#include <iostream>
+#include <stdio.h>
+#include <boost/process.hpp>
+
+
+void runTask(int level, int unit, int task) {
+  std::ostringstream os;
+  os
+    << "cmake-build-debug/cpp1/"
+    << "level" << level
+    << "/unit" << unit
+    << "/"
+    << "level" << level << "_unit" << unit << "_task" << task;
+
+  std::cerr << "Opening: " << os.str();
+
+  FILE* p = popen(os.str().c_str(), "r");
+//  std::ifstream is(p);
+}
 
 
 TEST_CASE("Utils1") {
   SECTION("Sec1") {
+    runTask(1, 2, 1);
+
     REQUIRE(7 == 7);
   }
 }
