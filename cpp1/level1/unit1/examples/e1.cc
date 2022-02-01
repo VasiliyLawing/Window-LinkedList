0596690c9844520772bda6b539cccc04c29b9dcd
bily101 <dada@flaofl.com>
2022-01-31 21:16:11 -0500

    Finished everything
    Files : unit 1, example 1, example 2

--- cpp1/level1/unit1/examples/e1.cc
+++ cpp1/level1/unit1/examples/e1.cc
@@ -9,28 +9,40 @@ struct Frac {
     Frac operator * (const Frac& f2){
         return Frac{m * f2.m, n * f2.n};
     }
+    Frac operator / (const Frac& f2){
+        return Frac{m * f2.n, n*f2.m };
+    }
+
+
+    Frac simplify () {
+        while ((m % 2 == 0 && n % 2 == 0) || (m % 3 == 0 && n % 3 == 0)) {
+            // int m = m * n + m * f2.n;
+            // int denominator = f2.n* n;
+
+            if (m % 2 == 0 && n % 2 == 0) {
+                m = m / 2;
+                n = n / 2;
+
+            } else if (m % 3 == 0 && n % 3 == 0) {
+                m = m / 3;
+                n = n / 3;
+            }
+        }
+        return Frac{m, n};
+
+    }
+
     Frac operator + (Frac& f2) {
         int numerator = f2.m * n + m * f2.n;
         int denominator = f2.n* n;
-
-        while ((numerator % 2 == 0 && denominator % 2 == 0) || (numerator % 3 == 0 && denominator % 3 == 0)) {
-           // int numerator = m * n + m * f2.n;
-           // int denominator = f2.n* n;
-
-            if (numerator % 2 == 0 && denominator % 2 == 0) {
-                numerator = numerator / 2;
-                denominator = denominator / 2;
-
-            } else if (numerator % 3 == 0 && denominator % 3 == 0) {
-                numerator = numerator / 3;
-                denominator = denominator / 3;
-
-            }
-        }
         return Frac{numerator, denominator};
 
+    }
+    Frac operator - (Frac& f2) {
+        int numerator = f2.m * n - m * f2.n;
+        int denominator = f2.n* n;
+        return Frac{numerator, denominator};
 
-    //    return Frac{numerator1 + numerator2, denominator1 + denominator2};
     }
 };
 
@@ -47,13 +59,14 @@ void Frac::print(std::ostream& os) const {
 
 
 int main()  {
-    Frac f1 = {3, 6};
-    Frac f2 = {2, 5};
+    Frac f1 = {1, 3};
+    Frac f2 = {2, 4};
 
     std::cout << "f1=" << f1 << std::endl;
     std::cout << "f2=" << f1 << std::endl;
 
-    Frac f12 = f1 + f2;
+    Frac f12 = f1 / f2;
+    f12.simplify();
     std::cout << "f12=" << f12 << std::endl;
 
 }
