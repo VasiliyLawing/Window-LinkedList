5ae5012b3ea69c5111cf73a9015b89b82b438324
bily101 <dada@flaofl.com>
2022-02-02 19:27:58 -0500

    Finished everything
    Files : unit 1, example 1, example 2

--- cpp1/level1/unit1/examples/e1.cc
+++ cpp1/level1/unit1/examples/e1.cc
@@ -4,69 +4,53 @@ struct Frac {
     int m;
     int n;
 
-    void print(std::ostream& os) const;
+    void print(std::ostream &os) const;
 
-    Frac operator * (const Frac& f2){
-        return Frac{m * f2.m, n * f2.n};
-    }
-    Frac operator / (const Frac& f2){
-        return Frac{m * f2.n, n*f2.m };
-    }
+    Frac operator*(const Frac &f2) const {return Frac{m * f2.m, n * f2.n}.simplify();}
 
+    Frac operator/(const Frac &f2) const {return Frac{m * f2.n, n * f2.m}.simplify();}
 
-    Frac simplify () {
-        while ((m % 2 == 0 && n % 2 == 0) || (m % 3 == 0 && n % 3 == 0)) {
-            // int m = m * n + m * f2.n;
-            // int denominator = f2.n* n;
+    Frac operator+(Frac &f2) const {return Frac{f2.m * n + m * f2.n, f2.n * n}.simplify();}
 
-            if (m % 2 == 0 && n % 2 == 0) {
-                m = m / 2;
-                n = n / 2;
+    Frac operator-(Frac &f2) const {return Frac{f2.m * n - m * f2.n, f2.n * n}.simplify();}
 
-            } else if (m % 3 == 0 && n % 3 == 0) {
-                m = m / 3;
-                n = n / 3;
-            }
-        }
-        return Frac{m, n};
+    Frac simplify();
 
-    }
-
-    Frac operator + (Frac& f2) {
-        int numerator = f2.m * n + m * f2.n;
-        int denominator = f2.n* n;
-        return Frac{numerator, denominator};
-
-    }
-    Frac operator - (Frac& f2) {
-        int numerator = f2.m * n - m * f2.n;
-        int denominator = f2.n* n;
-        return Frac{numerator, denominator};
-
-    }
 };
 
-inline std::ostream& operator<< (std::ostream& os, const Frac& f) {
+inline std::ostream &operator<<(std::ostream &os, const Frac &f) {
     f.print(os);
     return os;
 }
 
 
 // ------------------------- CC ---------------------------
-void Frac::print(std::ostream& os) const {
+void Frac::print(std::ostream &os) const {
     os << m << "/" << n << std::endl;
 }
 
+Frac Frac::simplify() {
+    while ((m % 2 == 0 && n % 2 == 0) || (m % 3 == 0 && n % 3 == 0)) {
+        if (m % 2 == 0 && n % 2 == 0) {
+            m = m / 2;
+            n = n / 2;
+        } else if (m % 3 == 0 && n % 3 == 0) {
+            m = m / 3;
+            n = n / 3;
+        }
+    }
+    return Frac{m, n};
+}
 
-int main()  {
-    Frac f1 = {1, 3};
-    Frac f2 = {2, 4};
+
+int main() {
+    Frac f1 = {7, 8};
+    Frac f2 = {9, 5};
 
     std::cout << "f1=" << f1 << std::endl;
     std::cout << "f2=" << f1 << std::endl;
 
-    Frac f12 = f1 / f2;
-    f12.simplify();
+    Frac f12 = f1 * f2;
     std::cout << "f12=" << f12 << std::endl;
 
 }
