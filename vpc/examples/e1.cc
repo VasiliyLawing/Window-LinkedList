168030af3b753fe6ef39f588ce3d8989dcf52a24
grok <grok@a.com>
2023-03-12 17:37:57 -0400

    .

--- vpc/examples/e1.cc
+++ vpc/examples/e1.cc
@@ -6,26 +6,17 @@ void dispayTest() {
     Display display("Virtual display", 1929, 1080);
     auto* displayMemory = display.getMemory();
 
-//    std::this_thread::sleep_for(std::chrono::seconds(10));
-
-
-    long timeMs = 0;
-    int delayMs = 1;
-
-    typeof(displayMemory) p = displayMemory;
-
     int color = 0;
 
     while(true) {
-        p = displayMemory;
+        typeof(displayMemory) p = displayMemory;
 
         for (int i = 0; i < display.getWidth() * display.getHeight(); ++i) {
 //            *p = 0xFF00FF00;
             *p = 0xFF00 << 8*2 | color << 8 | 0x00;
             p++;
 
-//        std::this_thread::sleep_for(std::chrono::nanoseconds (delayMs));
-            timeMs += delayMs;
+//        std::this_thread::sleep_for(std::chrono::nanoseconds (1));
 
             if (color == 0xFF)
                 color = 0;
