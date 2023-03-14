bf1cd32f5e4bd0528f61acb49e53d6442342d18d
grok <grok@a.com>
2023-03-13 20:45:56 -0400

    .

--- cpp1/level1/unit3/tasks/old/tasks.h
+++ cpp1/level1/unit3/tasks/old/tasks.h
@@ -1,16 +1,9 @@
 #pragma once
 
-
+/*
 namespace Cpp1::Unit2 {
   namespace Task1 {
-    int searchChar(const char* str, char ch);
-  }
-
-  namespace Task2 {
-    int replaceChar(char* str, char srcChar, char dstChar);
-  }
-
-  namespace Task3 {
-    void reverseStr(char* str);
+    int calculateSum(const char* str, char ch);
   }
 }
+*/
