df2aab2f82fcb7bcd0b5111bebd62268cf8f7080
grok <grok@a.com>
2023-03-17 16:48:14 -0400

    bg colors bug fixes.

--- vpc/examples/e2.cc
+++ vpc/examples/e2.cc
@@ -12,21 +12,26 @@ void dispayTest() {
     int fgColor = 1;
     int bgColor = 0;
 
-    for(int lineNum = 0; lineNum < 25; lineNum++) {
-        typeof(displayMemory) rowPtr = displayMemory + display.getResolution().columns * lineNum;
+    for(int row = 0; row < display.getResolution().rows; row++) {
+        typeof(displayMemory) rowPtr = displayMemory + display.getResolution().columns * row;
 
-        for (int col = 0; col < display.getResolution().columns && testText[col] != 0; col++) {
-            rowPtr[col] = bgColor << (8+4) | fgColor << 8 | testText[col];
+        const char* textPtr = testText;
 
-            if (fgColor < 16)
+        for (int col = 0; col < display.getResolution().columns; col++, textPtr++) {
+            if(*textPtr == 0)
+                textPtr = testText;
+
+            rowPtr[col] = bgColor << (8+4) | fgColor << 8 | *textPtr;
+
+            if (fgColor < 15)
                 fgColor++;
             else
                 fgColor = 1;
 
-            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+//            std::this_thread::sleep_for(std::chrono::milliseconds(100));
         }
 
-        if(bgColor < 16)
+        if(bgColor < 15)
             bgColor++;
         else
             bgColor = 0;
