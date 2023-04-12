c23e881acb7e55f1fc04bed8d10d6732262e9cad
grok <grok@a.com>
2023-04-12 03:17:48 -0400

    .

--- local-example.cmake
+++ local-example.cmake
@@ -1,2 +1,2 @@
-set(ANYSOLO_LOCAL_INC C:\\lib\\SDL2-2.26.4\\include\\ C:\\lib\\SDL2_image-2.6.3\\include)
-set(ANYSOLO_LOCAL_LIB C:\\lib\\SDL2-2.26.4\\lib\\x64\\ C:\\lib\\SDL2_image-2.6.3\\lib\\x64)
+set(ANYSOLO_LOCAL_INC /home/???/anysolo/lib-cpp-students/include)
+set(ANYSOLO_LOCAL_LIB /home/???/anysolo/lib-cpp-students/lib)
