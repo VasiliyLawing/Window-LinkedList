8456123d9bfa58ff1f151abe5ee96813f9cf25ac
bily101 <vblawing@gmail.com>
2023-08-08 20:18:28 -0500

    Fixed

---
+++ cpp1/level1/projects/myprj/test.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "cpp1/level1/projects/tst/mylib.hh"
+
+using namespace projects::Tst1;
+
+int main() {
+    std::cout << "Hello" << std::endl;
+
+    tstProjectFunc();
+}
